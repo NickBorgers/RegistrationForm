1ebc6e7cc86505b16d65fad3cbc1eed570c91daa
Vinit Wani <vinit.haunter@gmail.com>
2016-02-03 09:50:41 +0530

    Update reset-pwd-req.php

--- source/reset-pwd-req.php
+++ source/reset-pwd-req.php
@@ -18,7 +18,7 @@ if(isset($_POST['submitted']))
       <meta http-equiv='Content-Type' content='text/html; charset=utf-8'/>
       <title>Reset Password Request</title>
       <link rel="STYLESHEET" type="text/css" href="style/fg_membersite.css" />
-      <script type='text/javascript' src='scripts/gen_validatorv31.js'></script>
+      <script type='text/javascript' src='scripts/gen_validatorv4.js'></script>
 </head>
 <body>
 <!-- Form Code Start -->
